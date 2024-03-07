d6df25b1d4443de5b0fe0fe8f261749a319864ad
SaveKit <kittipit582@gmail.com>
2024-03-07 21:19:52 +0700

    final webapplication

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+#ชื่อตัวหใญ่
+st.title("Title")
+#ชื่อตัวเล็ก
+st.subheader("subheader")
+#ชื่อหัวเรื่องขนาดกลางๆ
+st.header("header")
+#ข้อความ
+st.text("text")
+# **..** ให้คำหนาขึ้น  *..*ให้คำเอียง 
+st.markdown("**mark** *down* ")
+#  #ตัวใหญ่ 
+st.markdown("# markdown ")
+st.markdown("[youtube](https://www.youtube.com/watch?v=ogG2fPLY544&list=PLa6CNrvKM5QU7AjAS90zCMIwi9RTFNIIW&index=4)")
+st.markdown("______")
+#ตัวสีเทาๆ
+st.caption("caption")
+
